a6c5f75bca282d4f2c9b9557f9d7d12e3b541db4
Lungu Victor <lungulll98@gmail.com>
2025-11-30 12:56:00 +0200

    Add Ex4

---
+++ src/Ex4.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Ex4 {
+    public static void main(String[] args) {
+
+        Scanner obj = new Scanner(System.in);
+        System.out.println("Introduceti trei numere: ");
+        int i = obj.nextInt();
+        int i1 = obj.nextInt();
+        int i2 = obj.nextInt();
+
+        if (i == i1 && i1 == i2) {
+            System.out.println("Toate numerele sunt egale.");
+        } else if (i != i1 && i != i2 && i1 != i2) {
+            System.out.println("Toate numerele sunt diferite.");
+        } else {
+            System.out.println("Nici nu sunt egale, nici diferite.");
+        }
+    }
+}
