6a4612beb2b03af1256e00a39348351cdd87a43f
Lungu Victor <lungulll98@gmail.com>
2025-11-30 12:34:59 +0200

    Add Ex1

---
+++ src/Ex1.java
@@ -0,0 +1,17 @@
+public class Ex1 {
+    public static void main(String[] args) {
+
+        byte b = 10;
+        short a = b;
+        System.out.println("Valoarea afisata: " + a);
+
+        int nr = 11;
+        double s = nr;
+        System.out.println("Valoarea afisata: " + s);
+
+        long nr1 = 11123123L;
+        int s1 = (int) nr1;
+        System.out.println("Valorea afista: " + nr1);
+
+    }
+}
